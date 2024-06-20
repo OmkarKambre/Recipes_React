e3182f7517a504962a2cd73ccd27fbfce4be6b9f
RishiPal22 <rishipallv22@gmail.com>
2024-06-20 12:48:37 +0530

    home page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+ReactDOM.render(
+  <>
+
+
+  <div className='header-container'>
+  <h1 className='t-header'>Recipes</h1>
+  </div>
+
+  <div className='nav'>
+  <h3>Home</h3>
+  <h3>Categories</h3>
+  <h3 className='a-header'>About Us</h3>
+  </div>
+  
+  </>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
