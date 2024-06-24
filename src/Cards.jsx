17a41ddb2c3891317f49db35e75733d6bb9c41f7
RishiPal22 <rishipallv22@gmail.com>
2024-06-24 19:21:17 +0530

    Card Props

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -8,7 +8,9 @@ function Cards(props){
               <img className="card-image" src={props.imgsrc}/>
               <h1>{props.category}</h1>
               <h4>{props.rname}</h4>
+              <p></p>
               <a href={props.link}>Watch Now</a>
+              <p className="read">Read</p>
             </div>
         </>
     );
