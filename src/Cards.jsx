5d230c6ae163adc06027397f3117c4eea02a7096
RishiPal22 <rishipallv22@gmail.com>
2024-07-01 20:08:48 +0530

    Flip Css

--- src/Cards.jsx
+++ src/Cards.jsx
@@ -12,18 +12,26 @@ function Cards(props) {
     return (
         <>
             <div className="mcard">
-                <img className="card-image" src={props.imgsrc} />
-                <h1>{props.category}</h1>
-                <h4>{props.rname}</h4>
-                <p></p>
-                <a href={props.link}>Watch Now</a>
-                <button
-                    className={"like-button " + (isLike ? "liked" : "")}
-                    onClick={onLikeButtonClick}
-                >
-                    {"👍"} | {like}
-                </button>
-                <p className="read">Read</p>
+                <div className="card-inner">
+                    <div className="card-front">
+                        <img className="card-image" src={props.imgsrc} />
+                        <h1>{props.category}</h1>
+                        <h4>{props.rname}</h4>
+                        <p></p>
+                        <button
+                            className={"like-button " + (isLike ? "liked" : "")}
+                            onClick={onLikeButtonClick}
+                        >
+                            {"👍"} | {like}
+                        </button>
+                    </div>
+                    <div className="card-back">
+                        <h1 style={{color: "black"}}>Description</h1>
+                        <center><p className="read">{props.info}</p></center>
+                        <a href={props.link} target="_blank" className="watchnow">Watch Now</a>
+                    </div>
+
+                </div>
             </div>
         </>
     );
